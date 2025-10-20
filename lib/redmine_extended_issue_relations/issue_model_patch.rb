0ce4bfefc6284ff95566558b1481088115cfc8ac
Christophe Hartmann <christophe.hartmann@axn.fr>
2025-10-20 15:01:42 +0200

    #21065 : feat - change status wo admin role

---
+++ lib/redmine_extended_issue_relations/issue_model_patch.rb
@@ -0,0 +1,69 @@
+
+module RedmineExtendedIssueRelations
+  module IssueModelPatch
+    def self.included(base)
+      base.class_eval do
+         prepend ClassMethods
+      end
+    end
+
+    module ClassMethods
+      
+      def workflow_rule_by_attribute(user=nil)
+        return @workflow_rule_by_attribute if @workflow_rule_by_attribute && user.nil?
+
+        roles = roles_for_axn_workflow(user || User.current)
+        return {} if roles.empty?
+
+        result = {}
+        workflow_permissions =
+          WorkflowPermission.where(
+            :tracker_id => tracker_id, :old_status_id => status_id,
+            :role_id => roles.map(&:id)
+          ).to_a
+        if workflow_permissions.any?
+          workflow_rules = workflow_permissions.inject({}) do |h, wp|
+            h[wp.field_name] ||= {}
+            h[wp.field_name][wp.role_id] = wp.rule
+            h
+          end
+          fields_with_roles = {}
+          IssueCustomField.where(:visible => false).
+            joins(:roles).pluck(:id, "role_id").
+              each do |field_id, role_id|
+            fields_with_roles[field_id] ||= []
+            fields_with_roles[field_id] << role_id
+          end
+          roles.each do |role|
+            fields_with_roles.each do |field_id, role_ids|
+              unless role_ids.include?(role.id)
+                field_name = field_id.to_s
+                workflow_rules[field_name] ||= {}
+                workflow_rules[field_name][role.id] = 'readonly'
+              end
+            end
+          end
+          workflow_rules.each do |attr, rules|
+            next if rules.size < roles.size
+
+            uniq_rules = rules.values.uniq
+            if uniq_rules.size == 1
+              result[attr] = uniq_rules.first
+            else
+              result[attr] = 'required'
+            end
+          end
+        end
+        @workflow_rule_by_attribute = result if user.nil?
+        result
+      end
+
+      def roles_for_axn_workflow(user)
+        roles = user.admin ? Role.all.to_a : user.roles_for_project(project)
+        roles.select(&:consider_workflow?)
+      end
+    end
+  end
+end
+
+AutoCompletesController.send(:include, RedmineExtendedIssueRelations::AutoCompletesControllerPatch)
